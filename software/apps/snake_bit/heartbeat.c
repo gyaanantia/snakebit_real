db48e633fe960cafd0aa0b8071abbf5dcc4ac4a8
Cam Brown <realcambrown@gmail.com>
2021-12-06 00:42:15 -0600

    heartbeat update

---
+++ software/apps/snake_bit/heartbeat.c
@@ -0,0 +1,40 @@
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include "nrfx_lpcomp.h"
+#include "nrf_lpcomp.h"
+
+#include "heartbeat.h"
+
+static void lpcomp_event_handler(nrf_lpcomp_event_t event) {
+    if(event == NRF_LPCOMP_EVENT_UP){
+        printf("Bing Bong!");
+    }
+}
+
+void heartbeat_setup() {
+
+    nrf_lpcomp_config_t hal_ = {
+            .detection = NRF_LPCOMP_DETECT_UP,
+            .reference = NRF_LPCOMP_REF_SUPPLY_7_8
+    };
+
+    nrf_lpcomp_input_t input_ = NRF_LPCOMP_INPUT_3;
+
+    nrfx_lpcomp_config_t p_config_ = {
+            .hal = hal_,
+            .input = input_,
+            .interrupt_priority = 1
+    };
+
+    // initialize the LPCOMP driver
+    nrfx_lpcomp_init(&p_config_, lpcomp_event_handler);
+
+    // enable the LPCOMP peripheral and interrupts
+    nrfx_lpcomp_enable();
+}
+
+
+
---
+++ software/apps/snake_bit/heartbeat.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "nrf_lpcomp.h"
+#include "nrfx_lpcomp.h"
+
+//extern nrfx_lpcomp_config_t p_config_;
+//
+//extern nrfx_lpcomp_event_handler_t event_handler_;
+
+void heartbeat_setup(void);
+
+
+
